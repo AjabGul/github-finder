5b511954fcb4a027ff7bdbd848c3aee67e029a59
Ajab Gul <kakar.ajabgul@gmail.com>
2021-03-28 14:57:46 +0500

    Github finder app by react completed
    react app named Github finder

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h3>GitHub Finder App</h3>
+            <p>This app will help us to find GitHub Users and thier repository.</p>
+        </Fragment>
+    )
+}
+
+export default About;
